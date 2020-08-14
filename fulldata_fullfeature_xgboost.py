8b5a721e362eb81216845cedb6e1d6f342fb72d7
LiuHDme <jason986907@gmail.com>
2020-08-15 00:27:49 +0800

    optimize code structure

--- fulldata_fullfeature_xgboost.py
+++ fulldata_fullfeature_xgboost.py
@@ -7,11 +7,17 @@ from sklearn.model_selection import ParameterGrid
 from sklearn.metrics import mean_squared_error, mean_absolute_error
 from datetime import date, timedelta
 from tqdm import tqdm
+import xgboost as xgb
+from xgboost import XGBRegressor
+from xgboost import plot_importance
 
+# read data
 train = pd.read_csv('Data/train.csv')
 test = pd.read_csv('Data/test.csv')
-train.columns = ['timestamp', 'year', 'month', 'day', 'hour', 'minute', 'second', 'temp_out', 'hum_out', 'air_out', 'hum_in', 'air_in', 'temp_in']
-test.columns = ['timestamp', 'year', 'month', 'day', 'hour', 'minute', 'second', 'temp_out', 'hum_out', 'air_out', 'hum_in', 'air_in']
+train.columns = ['timestamp', 'year', 'month', 'day', 'hour', 'minute', 'second', 'temp_out', 'hum_out', 'air_out',
+                 'hum_in', 'air_in', 'temp_in']
+test.columns = ['timestamp', 'year', 'month', 'day', 'hour', 'minute', 'second', 'temp_out', 'hum_out', 'air_out',
+                'hum_in', 'air_in']
 train['time'] = pd.to_datetime(train[['year', 'month', 'day', 'hour', 'minute']])
 test['time'] = pd.to_datetime(test[['year', 'month', 'day', 'hour', 'minute']])
 
@@ -25,7 +31,7 @@ for day in range(14, 32):
             date['day'].append(day)
             date['hour'].append(hour)
             date['minute'].append(minute)
-            
+
 for day in range(1, 14):
     for hour in range(0, 24):
         for minute in range(0, 60):
@@ -42,6 +48,7 @@ full_train = full_date.loc[(full_date.time >= '2019-3-14 01') & (full_date.time 
 train = pd.merge(full_train, train, on=['year', 'month', 'day', 'hour', 'minute', 'time'], how='left')
 train.drop_duplicates(['month', 'day', 'hour', 'minute'], inplace=True)
 train.reset_index(inplace=True, drop=True)
+
 
 # fill na
 # train
@@ -68,8 +75,9 @@ def fill_na(row, col):
                     # 否则直接找上一个非空值
                     pre = train.loc[(train.time < row.time) & (pd.notna(train[col]))]
                     return pre[col].values[0]
-            
+
     return row[col]
+
 
 train.drop_duplicates(['month', 'day', 'hour', 'minute'], inplace=True)
 
@@ -78,6 +86,7 @@ for feat in tqdm(['temp_out', 'hum_out', 'air_out', 'hum_in', 'air_in', 'temp_in
 
 train['target'] = train['temp_in'] - train['temp_out']
 
+
 # test
 def avg_pre_next(row, col):
     if pd.isna(row[col]):
@@ -85,6 +94,7 @@ def avg_pre_next(row, col):
         next_val = test.loc[test.time > row.time].iloc[0][col]
         return (pre_val + next_val) / 2
     return row[col]
+
 
 for feat in tqdm(['temp_out', 'hum_out', 'air_out', 'hum_in', 'air_in']):
     test[feat] = test.apply(avg_pre_next, axis=1, args=(feat,))
@@ -96,11 +106,11 @@ matrix = pd.concat([train, test], axis=0, ignore_index=True)
 features = ['temp_out', 'hum_out', 'air_out', 'hum_in', 'air_in']
 group_feats = []
 for f in tqdm(features):
-    matrix['MDH_{}_medi'.format(f)] = matrix.groupby(['month','day','hour'])[f].transform('median')
-    matrix['MDH_{}_mean'.format(f)] = matrix.groupby(['month','day','hour'])[f].transform('mean')
-    matrix['MDH_{}_max'.format(f)] = matrix.groupby(['month','day','hour'])[f].transform('max')
-    matrix['MDH_{}_min'.format(f)] = matrix.groupby(['month','day','hour'])[f].transform('min')
-    matrix['MDH_{}_std'.format(f)] = matrix.groupby(['month','day','hour'])[f].transform('std')
+    matrix['MDH_{}_medi'.format(f)] = matrix.groupby(['month', 'day', 'hour'])[f].transform('median')
+    matrix['MDH_{}_mean'.format(f)] = matrix.groupby(['month', 'day', 'hour'])[f].transform('mean')
+    matrix['MDH_{}_max'.format(f)] = matrix.groupby(['month', 'day', 'hour'])[f].transform('max')
+    matrix['MDH_{}_min'.format(f)] = matrix.groupby(['month', 'day', 'hour'])[f].transform('min')
+    matrix['MDH_{}_std'.format(f)] = matrix.groupby(['month', 'day', 'hour'])[f].transform('std')
 
     group_feats.append('MDH_{}_medi'.format(f))
     group_feats.append('MDH_{}_mean'.format(f))
@@ -125,9 +135,9 @@ for f in features:
         tmp.columns = ['hour', 'hit_{}_mean'.format(f)]
         tmp['dt'] = t
         tmp_df = tmp_df.append(tmp)
-    
+
     matrix = matrix.merge(tmp_df, on=['dt', 'hour'], how='left')
-    
+
 matrix = matrix.fillna(method='bfill')
 
 # lag features
@@ -135,9 +145,9 @@ features = ['temp_out', 'hum_out', 'air_out', 'hum_in', 'air_in']
 lags = [1, 24]
 
 for l in tqdm(lags):
-    tmp = matrix[features+['time']].copy()
+    tmp = matrix[features + ['time']].copy()
     tmp.time += pd.Timedelta(str(l) + ' hour')
-    col_names = ['lag_'+str(l)+'_'+f for f in features]
+    col_names = ['lag_' + str(l) + '_' + f for f in features]
     tmp.columns = col_names + ['time']
     matrix = pd.merge(matrix, tmp, on='time', how='left')
     del tmp
@@ -147,67 +157,68 @@ matrix.fillna(method='ffill', inplace=True)
 # Trend
 for feat in features:
     for n in lags:
-        matrix['trend_'+str(n)+'_'+feat] = matrix[feat] - matrix['lag_'+str(n)+'_'+feat]
+        matrix['trend_' + str(n) + '_' + feat] = matrix[feat] - matrix['lag_' + str(n) + '_' + feat]
 matrix.fillna(method='ffill', inplace=True)
 
 # Bin features
 features = ['temp_out', 'hum_out', 'air_out', 'hum_in', 'air_in']
 for f in features:
-    matrix[f+'_20_bin'] = pd.cut(matrix[f], 20, duplicates='drop').apply(lambda x:x.left).astype(int)
-    matrix[f+'_50_bin'] = pd.cut(matrix[f], 50, duplicates='drop').apply(lambda x:x.left).astype(int)
-    matrix[f+'_100_bin'] = pd.cut(matrix[f], 100, duplicates='drop').apply(lambda x:x.left).astype(int)
-    matrix[f+'_200_bin'] = pd.cut(matrix[f], 200, duplicates='drop').apply(lambda x:x.left).astype(int)
-    
+    matrix[f + '_20_bin'] = pd.cut(matrix[f], 20, duplicates='drop').apply(lambda x: x.left).astype(int)
+    matrix[f + '_50_bin'] = pd.cut(matrix[f], 50, duplicates='drop').apply(lambda x: x.left).astype(int)
+    matrix[f + '_100_bin'] = pd.cut(matrix[f], 100, duplicates='drop').apply(lambda x: x.left).astype(int)
+    matrix[f + '_200_bin'] = pd.cut(matrix[f], 200, duplicates='drop').apply(lambda x: x.left).astype(int)
+
 features_20_bin = [f + '_20_bin' for f in features]
 for f1 in tqdm(features_20_bin):
     for f2 in features:
-        matrix['{}_{}_medi'.format(f1,f2)] = matrix.groupby([f1])[f2].transform('median')
-        matrix['{}_{}_mean'.format(f1,f2)] = matrix.groupby([f1])[f2].transform('mean')
-        matrix['{}_{}_max'.format(f1,f2)] = matrix.groupby([f1])[f2].transform('max')
-        matrix['{}_{}_min'.format(f1,f2)] = matrix.groupby([f1])[f2].transform('min')
+        matrix['{}_{}_medi'.format(f1, f2)] = matrix.groupby([f1])[f2].transform('median')
+        matrix['{}_{}_mean'.format(f1, f2)] = matrix.groupby([f1])[f2].transform('mean')
+        matrix['{}_{}_max'.format(f1, f2)] = matrix.groupby([f1])[f2].transform('max')
+        matrix['{}_{}_min'.format(f1, f2)] = matrix.groupby([f1])[f2].transform('min')
 
 features_50_bin = [f + '_50_bin' for f in features]
 for f1 in tqdm(features_50_bin):
     for f2 in features:
-        matrix['{}_{}_medi'.format(f1,f2)] = matrix.groupby([f1])[f2].transform('median')
-        matrix['{}_{}_mean'.format(f1,f2)] = matrix.groupby([f1])[f2].transform('mean')
-        matrix['{}_{}_max'.format(f1,f2)] = matrix.groupby([f1])[f2].transform('max')
-        matrix['{}_{}_min'.format(f1,f2)] = matrix.groupby([f1])[f2].transform('min')
+        matrix['{}_{}_medi'.format(f1, f2)] = matrix.groupby([f1])[f2].transform('median')
+        matrix['{}_{}_mean'.format(f1, f2)] = matrix.groupby([f1])[f2].transform('mean')
+        matrix['{}_{}_max'.format(f1, f2)] = matrix.groupby([f1])[f2].transform('max')
+        matrix['{}_{}_min'.format(f1, f2)] = matrix.groupby([f1])[f2].transform('min')
 
 features_100_bin = [f + '_100_bin' for f in features]
 for f1 in tqdm(features_100_bin):
     for f2 in features:
-        matrix['{}_{}_medi'.format(f1,f2)] = matrix.groupby([f1])[f2].transform('median')
-        matrix['{}_{}_mean'.format(f1,f2)] = matrix.groupby([f1])[f2].transform('mean')
-        matrix['{}_{}_max'.format(f1,f2)] = matrix.groupby([f1])[f2].transform('max')
-        matrix['{}_{}_min'.format(f1,f2)] = matrix.groupby([f1])[f2].transform('min')
+        matrix['{}_{}_medi'.format(f1, f2)] = matrix.groupby([f1])[f2].transform('median')
+        matrix['{}_{}_mean'.format(f1, f2)] = matrix.groupby([f1])[f2].transform('mean')
+        matrix['{}_{}_max'.format(f1, f2)] = matrix.groupby([f1])[f2].transform('max')
+        matrix['{}_{}_min'.format(f1, f2)] = matrix.groupby([f1])[f2].transform('min')
 
 features_200_bin = [f + '_200_bin' for f in features]
 for f1 in tqdm(features_200_bin):
     for f2 in features:
-        matrix['{}_{}_medi'.format(f1,f2)] = matrix.groupby([f1])[f2].transform('median')
-        matrix['{}_{}_mean'.format(f1,f2)] = matrix.groupby([f1])[f2].transform('mean')
-        matrix['{}_{}_max'.format(f1,f2)] = matrix.groupby([f1])[f2].transform('max')
-        matrix['{}_{}_min'.format(f1,f2)] = matrix.groupby([f1])[f2].transform('min')
+        matrix['{}_{}_medi'.format(f1, f2)] = matrix.groupby([f1])[f2].transform('median')
+        matrix['{}_{}_mean'.format(f1, f2)] = matrix.groupby([f1])[f2].transform('mean')
+        matrix['{}_{}_max'.format(f1, f2)] = matrix.groupby([f1])[f2].transform('max')
+        matrix['{}_{}_min'.format(f1, f2)] = matrix.groupby([f1])[f2].transform('min')
 
 # Rolling features
 features = ['temp_out', 'hum_out', 'air_out', 'hum_in', 'air_in']
-lags = [1,3,6,12,24]
+lags = [1, 3, 6, 12, 24]
 matrix.set_index('time', inplace=True)
 
 for feat in tqdm(features):
     for l in lags:
         lag_hour = str(l) + 'h'
-        matrix['mean_'+str(l)+'_hours_'+feat] = matrix[feat].rolling(lag_hour).mean()
-        matrix['median_'+str(l)+'_hours_'+feat] = matrix[feat].rolling(lag_hour).median()
-        matrix['max_'+str(l)+'_hours_'+feat] = matrix[feat].rolling(lag_hour).max()
-        matrix['min_'+str(l)+'_hours_'+feat] = matrix[feat].rolling(lag_hour).min()
-        matrix['std_'+str(l)+'_hours_'+feat] = matrix[feat].rolling(lag_hour).std()
-        matrix['skew_'+str(l)+'_hours_'+feat] = matrix[feat].rolling(lag_hour).skew()
-        matrix['q1_'+str(l)+'_hours_'+feat] = matrix[feat].rolling(lag_hour).quantile(quantile=0.25)
-        matrix['q3_'+str(l)+'_hours_'+feat] = matrix[feat].rolling(lag_hour).median(quantile=0.75)
-        matrix['var_'+str(l)+'_hours_'+feat] = matrix['std_'+str(l)+'_hours_'+feat] / matrix['mean_'+str(l)+'_hours_'+feat]
-        
+        matrix['mean_' + str(l) + '_hours_' + feat] = matrix[feat].rolling(lag_hour).mean()
+        matrix['median_' + str(l) + '_hours_' + feat] = matrix[feat].rolling(lag_hour).median()
+        matrix['max_' + str(l) + '_hours_' + feat] = matrix[feat].rolling(lag_hour).max()
+        matrix['min_' + str(l) + '_hours_' + feat] = matrix[feat].rolling(lag_hour).min()
+        matrix['std_' + str(l) + '_hours_' + feat] = matrix[feat].rolling(lag_hour).std()
+        matrix['skew_' + str(l) + '_hours_' + feat] = matrix[feat].rolling(lag_hour).skew()
+        matrix['q1_' + str(l) + '_hours_' + feat] = matrix[feat].rolling(lag_hour).quantile(quantile=0.25)
+        matrix['q3_' + str(l) + '_hours_' + feat] = matrix[feat].rolling(lag_hour).median(quantile=0.75)
+        matrix['var_' + str(l) + '_hours_' + feat] = matrix['std_' + str(l) + '_hours_' + feat] / matrix[
+            'mean_' + str(l) + '_hours_' + feat]
+
 matrix.reset_index(inplace=True)
 
 # Save data
@@ -218,7 +229,7 @@ data = pd.read_pickle('data.pkl')
 
 features_to_drop = ['timestamp', 'year', 'second', 'time', 'target', 'temp_in']
 
-num = int(len(train)*0.8)
+num = int(len(train) * 0.8)
 X_train = data.iloc[:num].drop(features_to_drop, axis=1)
 y_train = data.iloc[:num]['target']
 # y_train = data.iloc[:num]['temp_in'] - pred_train_lr[:num]
@@ -227,27 +238,22 @@ y_val = data.iloc[num:len(train)]['target']
 # y_val = data.iloc[num:len(train)]['temp_in'] - pred_train_lr[num:len(train)]
 X_test = data.loc[data.time >= '2019-4-3 01'].drop(features_to_drop, axis=1)
 
-# Xgboost
-import xgboost as xgb
-from xgboost import XGBRegressor
-from xgboost import plot_importance
-
 model = XGBRegressor(max_depth=8,
                      n_estimators=50000,
-                     min_child_weight=5, 
-                     colsample_bytree=0.5, 
-                     subsample=0.5, 
+                     min_child_weight=5,
+                     colsample_bytree=0.5,
+                     subsample=0.5,
                      eta=0.001,
-                     seed=2020, 
+                     seed=2020,
                      n_jobs=2)
-model.fit(X_train, 
-          y_train, 
-          eval_metric='rmse', 
-          eval_set=[(X_train, y_train), (X_val, y_val)], 
-          verbose=500, 
+model.fit(X_train,
+          y_train,
+          eval_metric='rmse',
+          eval_set=[(X_train, y_train), (X_val, y_val)],
+          verbose=500,
           early_stopping_rounds=1000)
 
 pred_test_xgb = model.predict(X_test, ntree_limit=model.best_ntree_limit)
-submission = pd.DataFrame({'time': test.timestamp, 
+submission = pd.DataFrame({'time': test.timestamp,
                            'temperature': test.temp_out + pred_test_xgb})
 submission.to_csv('submissions/xgb_submission.csv', index=False)
